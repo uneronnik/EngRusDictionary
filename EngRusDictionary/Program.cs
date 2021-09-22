3e55079ddb62ad8739dfeb9a9b258e5e85efb7c8
uneronnik <NIKI.FOMENKO.7@GMAIL.COM>
2021-09-22 23:27:56 +0300

    2.0

--- EngRusDictionary/Program.cs
+++ EngRusDictionary/Program.cs
@@ -14,6 +14,7 @@ namespace EngRusDictionary
             LanguageDictionary dictionary = new LanguageDictionary();
             dictionary.AddTranslation("Россия", "Russia");
             dictionary.AddTranslation("Америка", "America");
+            dictionary.AddTranslation("США", "America");
             dictionary.AddTranslation("USA", "Америка");
             dictionary.AddTranslation("Китай", "China");
             
@@ -21,16 +22,20 @@ namespace EngRusDictionary
             {
                 Console.WriteLine($"Россия - {translation}");
             }
-            
-            
+            Console.WriteLine();
+
             foreach (var translation in dictionary.GetTranslations("America"))
             {
                 Console.WriteLine($"America - {translation}");
             }
+            Console.WriteLine();
+
             foreach (var translation in dictionary.GetTranslations("Америка"))
             {
                 Console.WriteLine($"Америка - {translation}");
             }
+            Console.WriteLine();
+
             Console.ReadKey(); 
         }
     }
